80564937f46a3153879a46a60f95dae3f9da0d9a
guocl <guocl85@163.com>
2024-04-07 11:12:42 +0800

    Dev 添加test command用例

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,8 +6,22 @@ import os
 
 sys.path.insert(0, project_rootpath := os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
-from FlexiDNS.tomlconfigure import loader_config
+from FlexiDNS.tomlconfigure import loader_config, share_objects
 loader_config(os.path.join(project_rootpath, 'etc', 'flexidns', 'config_none.toml'))
+
+
+if os.path.exists(share_objects.PIDFILE):
+
+    with open(share_objects.PIDFILE, 'r') as f:
+        try:
+            PGID = os.getpgid(int(f.read()))
+        except (OSError, ProcessLookupError, ValueError):
+            if os.path.exists(share_objects.SOCKFILE):
+                os.remove(share_objects.SOCKFILE)
+            os.remove(share_objects.PIDFILE)
+else:
+    if os.path.exists(share_objects.SOCKFILE):
+        os.remove(share_objects.SOCKFILE)
 
 """
 单元测试笔记
--- tests/test_command.py
+++ tests/test_command.py
@@ -4,17 +4,17 @@
 import unittest
 from collections import deque
 from unittest.mock import patch, MagicMock, Mock
-import pickle as cpickle
 
 from dnslib import *
 
-from FlexiDNS.command import CacheOperate, pickle, socket
+from FlexiDNS.command import CacheOperate, os
 
 
 class Test_Command(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        os.path.exists = Mock(return_value=True)
         cls.command = CacheOperate()
 
     def setUp(self) -> None:
@@ -23,8 +23,9 @@ class Test_Command(unittest.TestCase):
         self.dnslabel = DNSLabel(self.qname)
         self.rr = RR(self.qname, ttl=60, rdata=A('192.168.1.1'))
 
+    @patch('FlexiDNS.command.pickle.dumps')
     @patch('FlexiDNS.command.CacheOperate.data_recv')
-    def test_01_cacheshow_dict(self, data_recv_mock):
+    def test_01_cacheshow_dict(self, data_recv_mock, pickle_dumps_mock):
         import pickle as cpickle
         cmd = {'cache': {'cmd': 'show', 'all': True, 'qname': None}}
         message = cpickle.dumps(cmd)
@@ -34,14 +35,16 @@ class Test_Command(unittest.TestCase):
 
         data_recv_mock.return_value=recv_data
         CacheOperate.unloads = MagicMock(return_value=[mmdata])
-        pickle.dumps = MagicMock(return_value=message)
+        pickle_dumps_mock.return_value=message
 
         with patch('builtins.print') as print_mock:
             self.command.cache(cmd)
             print_mock.assert_called_once_with(result)
 
+    @patch('FlexiDNS.command.pickle.dumps')
     @patch('FlexiDNS.command.CacheOperate.data_recv')
-    def test_02_cacheshow_list(self, data_recv_mock):
+    def test_02_cacheshow_list(self, data_recv_mock, pickle_dumps_mock):
+        import pickle as cpickle
         cmd = {'cache': {'cmd': 'show', 'all': False, 'qname': [self.qname]}}
         message = cpickle.dumps(cmd)
         recv_data = {'type': 'cache', 'argparse': 'show', 'data_length': 30, 'data': b''}
@@ -50,21 +53,42 @@ class Test_Command(unittest.TestCase):
 
         data_recv_mock.return_value=recv_data
         CacheOperate.unloads = MagicMock(return_value=[mmdata])
-        pickle.dumps = MagicMock(return_value=message)
+        pickle_dumps_mock.return_value=message
 
         with patch('builtins.print') as print_mock:
             self.command.cache(cmd)
             print_mock.assert_called_once_with(result)
 
-    @patch('FlexiDNS.command.CacheOperate.data_recv')
-    def test_03_history(self, data_recv_mock):
+class Test_Command_history(unittest.TestCase):
 
-        cmd = {'history': {'all': True}}
-        history_data = deque([(1712404645.3993819, '::1', DNSLabel(self.qname))], maxlen=500)
+    @classmethod
+    def setUpClass(cls):
+        os.path.exists = Mock(return_value=True)
+        cls.command = CacheOperate()
+
+    def setUp(self) -> None:
+        import pickle
+        # 因为pickle在测试用例中，会被覆盖。所以在setUp中使用
+        self.command = Test_Command_history.command
+        self.qname = 'www.flexidns.com'
+        self.dnslabel = DNSLabel(self.qname)
+        self.rr = RR(self.qname, ttl=60, rdata=A('192.168.1.1'))
+        self.history = deque([(1712404645.3993819, '::1', DNSLabel(self.qname))], maxlen=500)
+        self.history_data = pickle.dumps(self.history)
+        self.cmd = {'history': {'all': True}}
+
+    @patch('FlexiDNS.command.pickle.loads')
+    @patch('FlexiDNS.command.pickle.dumps')
+    @patch('FlexiDNS.command.CacheOperate.data_recv')
+    def test_03_history(self, data_recv_mock, pickle_dumps_mock, pickle_loads_mock):
+
+        pickle_dumps_mock.return_value=None
+        pickle_loads_mock.return_value=self.history
+
         data_recv_mock.return_value = {
-            'type': None, 'argparse': None, 'data_length': 127, 'data': cpickle.dumps(history_data)}
+            'type': None, 'argparse': None, 'data_length': 127, 'data': self.history_data}
         with patch('builtins.print') as print_mock:
-            self.command.history(cmd)
+            self.command.history(self.cmd)
             print_mock.assert_called()
 
 if __name__ == '__main__':
